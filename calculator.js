2ecd2e51493ca4284e5e8717830cab1998c985f7
Hamza Rafique <hamzarafique964@gmail.com>
2026-08-26 09:50:59 +0000

    Add calculateDiscount function

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-// calculator.js
+
 export function calculateTotal(items) {
   let total = 0;
   for (let i = 0; i < items.length; i++) {
@@ -12,4 +12,10 @@ export function applyDiscount(price, discount) {
     throw new Error('Discount cannot exceed 50%');
   }
   return price - (price * discount / 100);
+}
+export function calculateDiscount(price, percentage) {
+  if (percentage < 0 || percentage > 100) {
+    throw new Error('Percentage must be between 0 and 100');
+  }
+  return price * (percentage / 100);
 }
